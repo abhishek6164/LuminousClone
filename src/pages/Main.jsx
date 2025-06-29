56a7018b47a40ee9309edf39e750f0eecf44e10d
“Abhishek <abhishekpipriye2507@gmail.com>
2025-06-29 13:16:44 +0530

    1st commit

---
+++ src/pages/Main.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import MainImg from '../assets/main.webp';
+
+const Main = () => {
+    return (
+        <div className="relative w-full  h-full  overflow-hidden px-8 py-8">
+            <img
+                src={MainImg}
+                alt="Main"
+                className="w-full rounded-[40px] h-[700px] object-cover"
+            />
+
+            {/* Overlay Text */}
+            <div className="absolute top-[80%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center max-w-5xl">
+                <p className="text-sm  md:text-lg font-medium mb-2 opacity-60">
+                    Red Light Therapy: proven, safe, and non-invasive
+                </p>
+                <h1 className="text-5xl md:text-6xl lg:text-6xl noto-sans font-bold leading-tight">
+                    Your cells, supercharged
+                </h1>
+
+            </div>
+        </div>
+    );
+};
+
+export default Main;
